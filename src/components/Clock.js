e3f2909d672d97a3a1b4e521ade2d5cb520171bc
MOHAMED ALI FAWZI <76850143+MohamedAli00949@users.noreply.github.com>
2022-08-10 16:03:51 +0200

    make roller in clock design

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -2,23 +2,34 @@ import React from "react";
 
 const Clock = () => {
     const nums = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55];
-    const arrows = [1, 2, 3, 4, 5].concat(...Array.from({ length: 11 }, () => [1, 2, 3, 4, 5].map(num => num * 2)));
+    const arrows = [1, 2, 3, 4, 5].concat(...Array.from({ length: 5 }, () => [1, 2, 3, 4, 5]));
+    let roteNum = 0;
+
+    const generateRotation = () => {
+        const rotation = roteNum * 6;
+        roteNum++;
+        return rotation;
+    }
 
     return (
         <div className="clock-container">
             <div className="clock">
-                {nums.map((item, index) => (
-                    <div style={{ "--i": `${index}` }} className="number" key={item}>
+                {nums.map((item, index1) => (
+                    <div style={{ "--i": `${index1}` }} className="number" key={item}>
                         <p>
                             {item}
                         </p>
                     </div>
                 ))}
-            </div>
-            <div>
-                {arrows.map((item, index) => (
-                    <span key={index} className={`arrow${index % 5 === 0 ? " main" : ""}`}></span>
-                ))}
+                <div className="arrows">
+                    {arrows.map((item, index2) => (
+                        <span key={index2} style={{ "--rotation": generateRotation() }} className={`arrow${index2 % 5 === 0 ? " main" : ""}`}></span>
+                    ))}
+                </div>
+                <div className="roller-container">
+                    <div className="roll">
+                    </div>
+                </div>
             </div>
         </div>
     )
