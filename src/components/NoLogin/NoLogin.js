cd2e8b5c3a00b25f30490bc252b435d1e2738b6a
MOHAMED ALI FAWZI <76850143+MohamedAli00949@users.noreply.github.com>
2023-08-14 01:32:05 +0300

    fix: Redesign No login page

--- src/components/NoLogin/NoLogin.js
+++ src/components/NoLogin/NoLogin.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import Button from '../../utils/Button/Button';
+
 import './style.css';
 
 function NoLogin() {
+  const reload = () => {
+    window.location.reload();
+  }
+
   return (
     <div className='no-login'>
       <div className='container'>
@@ -11,12 +17,34 @@ function NoLogin() {
           <h1>Please, Login to use this feature</h1>
         </div>
         <div className='buttons'>
-          <Link to="/">
-            go home
-          </Link>
-          <Link to="/auth">
-            Login
-          </Link>
+          {window.location.pathname === '/' ? (
+            <Button
+              variant='contained'
+              onClick={reload}
+              style={{
+                marginInline: 20
+              }}
+            >
+              reload
+            </Button>
+          ) : (
+            <Button
+              component={Link}
+              to="/"
+              variant='contained'
+            >
+              go home
+            </Button>
+          )}
+          {(window.location.pathname === '/' || window.history.length > 2) && (
+            <Button
+              component={Link}
+              to={-1}
+              variant='outlined'
+            >
+              go back
+            </Button>
+          )}
         </div>
       </div>
     </div>
