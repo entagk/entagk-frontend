71588e65baa65697b2e5eb540732761fe6999b98
MOHAMED ALI FAWZI <76850143+MohamedAli00949@users.noreply.github.com>
2023-10-07 04:38:09 +0300

    style: tasks menu at the header

--- src/components/Tasks/TasksMenu/TasksMenu.js
+++ src/components/Tasks/TasksMenu/TasksMenu.js
@@ -44,42 +44,6 @@ const TasksMenu = ({ setMessage }) => {
     <>
       <Suspense fallback={
         <Loading
-          size="big"
-          strokeWidth="5"
-          color={"var(--main-color)"}
-          backgroud="transparent"
-        />
-      }>
-        {clear !== "" && (
-          <DeletePopup
-            type={
-              <>
-                <span>
-                  {
-                    clear === 'finished' ?
-                      'all finished tasks' :
-                      clear === 'act' ?
-                        'act promodors' :
-                        'all tasks'
-                  }
-                </span>
-              </>
-            }
-            onOk={
-              clear === 'finished' ?
-                handleClearFinishedTasks :
-                clear === 'act' ?
-                  handleClearAct :
-                  handleClearTasks
-            }
-            onCancel={
-              () => setClear("")
-            }
-          />
-        )}
-      </Suspense>
-      <Suspense fallback={
-        <Loading
           size="small"
           strokeWidth="5px"
           color={"#fff"}
@@ -87,60 +51,89 @@ const TasksMenu = ({ setMessage }) => {
           style={{ margin: 0 }}
         />
       }>
-        <Menu
-          open={openMenu}
-          setOpen={setOpenMenu}
-          MainButton={
-            <Button
-              aria-label="toggle the task list menu"
-              className="toggle-menu"
-              disabled={tasks?.length === 0}
-              startIcon={
-                <HiMenu />
+        <>
+          {clear !== "" && (
+            <DeletePopup
+              type={
+                <>
+                  <span>
+                    {
+                      clear === 'finished' ?
+                        'all finished tasks' :
+                        clear === 'act' ?
+                          'act promodors' :
+                          'all tasks'
+                    }
+                  </span>
+                </>
               }
-              variant="single-icon"
+              onOk={
+                clear === 'finished' ?
+                  handleClearFinishedTasks :
+                  clear === 'act' ?
+                    handleClearAct :
+                    handleClearTasks
+              }
+              onCancel={
+                () => setClear("")
+              }
             />
-          }
-          style={{
-            left: "0"
-          }}
-        >
-          <Button
-            type='button'
-            onClick={() => handleClear('act')}
-            aria-label="clear all act pomodoros tasks"
-            disabled={tasks?.filter(t => t.act > 0).length === 0}
-            startIcon={
-              <BsCheckLg />
+          )}
+          <Menu
+            open={openMenu}
+            setOpen={setOpenMenu}
+            MainButton={
+              <Button
+                aria-label="toggle the task list menu"
+                className="toggle-menu"
+                disabled={tasks?.length === 0}
+                startIcon={
+                  <HiMenu />
+                }
+                variant="single-icon"
+              />
             }
-            variant="none"
+            style={{
+              left: "0"
+            }}
           >
-            <>clear act pomodoros</>
-          </Button>
-          <Button
-            type='button'
-            onClick={() => handleClear('finished')}
-            aria-label="delete all finished tasks"
-            disabled={tasks?.filter(t => t.check).length === 0}
-            startIcon={
-              <MdDelete />
-            }
-            variant="none"
-          >
-            <>clear finished tasks</>
-          </Button>
-          <Button
-            type='button'
-            aria-label="clear all tasks"
-            onClick={() => handleClear('all')}
-            startIcon={
-              <MdDelete />
-            }
-            variant="none"
-          >
-            <>clear all tasks</>
-          </Button>
-        </Menu>
+            <Button
+              type='button'
+              onClick={() => handleClear('act')}
+              aria-label="clear all act pomodoros tasks"
+              disabled={tasks?.filter(t => t.act > 0).length === 0}
+              startIcon={
+                <BsCheckLg />
+              }
+              variant="none"
+            >
+              <>clear act pomodoros</>
+            </Button>
+            <Button
+              type='button'
+              onClick={() => handleClear('finished')}
+              aria-label="delete all finished tasks"
+              disabled={tasks?.filter(t => t.check).length === 0}
+              startIcon={
+                <MdDelete />
+              }
+              variant="none"
+            >
+              <>clear finished tasks</>
+            </Button>
+            <Button
+              type='button'
+              aria-label="clear all tasks"
+              onClick={() => handleClear('all')}
+              startIcon={
+                <MdDelete />
+              }
+              variant="none"
+            >
+              <>clear all tasks</>
+            </Button>
+          </Menu>
+        </>
       </Suspense>
     </>
   )
